47b9cbbcbc7a29435a04ee97411588dc69e1e1a4
vitali <vitali.slepnyov@gmail.com>
2022-12-04 19:29:14 +0300

    feat: добавить обработчики ошибок, логгеры и валидацию

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+
+module.exports = (req, res, next) => {
+  const devTokenSecret = 'dev-secret-key';
+  const cookieWithToken = req.cookies.jwt;
+
+  if (!cookieWithToken) {
+    return next(new UnauthorizedError('авторизация не пройдена'));
+  }
+  let payload;
+
+  try {
+    payload = jwt.verify(cookieWithToken, NODE_ENV === 'production' ? JWT_SECRET : devTokenSecret);
+  } catch {
+    return next(new UnauthorizedError('авторизация не пройдена'));
+  }
+
+  req.user = payload;
+  return next();
+};
